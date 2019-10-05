bb08bdd1eb81ea7c2cc373417fd48b5e2e33aec5
Alexander Williams <williams_alex@protonmail.com>
2019-10-05 04:32:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {:age => 53, :name => "Lord Montague"},
+      :matriarch => {:name => "Lady Montague", :age => 54},
+      :hero => {:name => "Romeo", :age => 15, :status => "alive"},
+      :hero_friends => []
       }, 
     :capulet => {
-      
+      :patriarch => {:name => "Lord Capulet", :age => 50},
+      :matriarch => {:name => "Lady Capulet", :age => 51},
+      :heroine => {:name => "Juliet", :age => 15, :status => "alive"},
+      :heroine_friends => []
       }}
-
-  
 end
