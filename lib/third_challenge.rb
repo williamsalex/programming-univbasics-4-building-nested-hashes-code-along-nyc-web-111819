69b0253381c91a0ef202763507d5e04ed057de57
Alexander Williams <williams_alex@protonmail.com>
2019-10-05 04:32:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -28,3 +28,17 @@ def third_challenge
 
   
 end
+
+  epic_tragedy = {
+    :montague => {
+      :patriarch => {:age => 53, :name => "Lord Montague"},
+      :matriarch => {:name => "Lady Montague", :age => 54},
+      :hero => {:name => "Romeo", :age => 15, :status => "alive"},
+      :hero_friends => []
+      }, 
+    :capulet => {
+      :patriarch => {:name => "Lord Capulet", :age => 50},
+      :matriarch => {:name => "Lady Capulet", :age => 51},
+      :heroine => {:name => "Juliet", :age => 15, :status => "alive"},
+      :heroine_friends => []
+      }}
