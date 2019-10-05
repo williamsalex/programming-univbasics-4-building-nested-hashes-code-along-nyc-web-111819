4663bb2d0e37b01ada33fe3db735a581ee429b14
Alexander Williams <williams_alex@protonmail.com>
2019-10-05 04:39:53 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,28 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+   :montague => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :hero => {
+        #your key/value pairs here
+        },
+      :hero_friends => []
+   }, 
+   :capulet => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :heroine => {
+        #your key/value pairs here
+        },
+      :heroine_friends => []
+   }
+  }
 end
