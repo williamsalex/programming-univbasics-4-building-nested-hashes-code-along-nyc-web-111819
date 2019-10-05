7d79fd7ce5a8fce741b86fe173374a5df370f6cc
Alexander Williams <williams_alex@protonmail.com>
2019-10-05 04:34:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,9 +7,9 @@ def third_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      :patriarch => {:name => "Lord Capulet", :age => 50},
-      :matriarch => {:name => "Lady Capulet", :age => 51},
-      :heroine => {:name => "Juliet", :age => 15, :status => "alive"},
+      :patriarch => {:name => "Lord Capulet", :age => "50"},
+      :matriarch => {:name => "Lady Capulet", :age => "51"},
+      :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
       :heroine_friends => []
       }}
 end
